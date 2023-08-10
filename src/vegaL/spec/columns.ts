f33945c706e9c677f9853032af4be6b35c0f25b3
Kiran Gadhave <kirangadhave2@gmail.com>
2023-08-10 00:28:01 -0600

    feat(rename-cols): added rename column interaction
    added gui to rename any column. updating vega based on it
    
    fixes #55

--- src/vegaL/spec/columns.ts
+++ src/vegaL/spec/columns.ts
@@ -1,12 +1,36 @@
+import { CalculateTransform } from 'vega-lite/build/src/transform';
 import { Interactions } from '../../interactions/types';
+import { addEncoding, getFieldNamesFromEncoding } from './encoding';
 import { VegaLiteSpecProcessor } from './processor';
 
 export function applyRenameColumn(
   vlProc: VegaLiteSpecProcessor,
-  _categoryAction: Interactions.RenameColumnAction
+  { prev_column_name, new_column_name }: Interactions.RenameColumnAction
 ) {
   vlProc.addLayer('BASE', spec => {
     const { transform = [] } = spec;
+
+    const calculate: CalculateTransform = {
+      calculate: `"datum[${prev_column_name}]"`,
+      as: `"${new_column_name}"`
+    };
+
+    transform.push(calculate);
+
+    const fields = getFieldNamesFromEncoding(
+      spec.encoding || {},
+      Object.keys(spec.encoding || {}) as any
+    );
+
+    Object.entries(fields as any).forEach(([key, val]: any[]) => {
+      if (val === prev_column_name) {
+        const encoding: any = spec.encoding || {};
+        spec.encoding = addEncoding(encoding, key, {
+          ...encoding[key],
+          title: new_column_name
+        });
+      }
+    });
 
     spec.transform = transform;
 
