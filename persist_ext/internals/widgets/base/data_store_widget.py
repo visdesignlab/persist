c24f75c00b70f6e7bb0be2f7b251ce7c530af0b3
NateLanza <natelanzadevelopment+github@gmail.com>
2025-03-13 18:14:51 -0600

    Bugfix: Properly saving state to file. Persist WORKING in VSCode!

--- persist_ext/internals/widgets/base/data_store_widget.py
+++ persist_ext/internals/widgets/base/data_store_widget.py
@@ -70,8 +70,8 @@ class DataStoreWidget(anywidget.AnyWidget):
     
     global dsw_global_data_store
     self.dsw_data_store = dsw_global_data_store.get(self.dsw_id, {})
-    self.observe(self._data_changed, names='dsw_data_store')
 
+  @observe('dsw_data_store')
   def _data_changed(self, _):
     # Ignore the initial value of dsw_data_store, which is set to work around a bug
     if len(self.dsw_data_store.keys()) == 1 and self.dsw_data_store.get('key') == 'value':
